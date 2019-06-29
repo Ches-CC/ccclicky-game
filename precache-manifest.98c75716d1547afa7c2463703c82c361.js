ad949c566380f00bb6881fed4446a3bc2f8f23cf
Ches-CC <chesley.calloway@gmail.com>
2019-06-29 10:28:19 -0700

    Updates

---
+++ precache-manifest.98c75716d1547afa7c2463703c82c361.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7892ec24604708671d0d0fae1d22539",
+    "url": "/ccclicky-game/index.html"
+  },
+  {
+    "revision": "f8c4a75c68d228567919",
+    "url": "/ccclicky-game/static/css/main.8925f97c.chunk.css"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/ccclicky-game/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "f8c4a75c68d228567919",
+    "url": "/ccclicky-game/static/js/main.f008c0c2.chunk.js"
+  },
+  {
+    "revision": "6be7dcc2c2d53cf2c01a",
+    "url": "/ccclicky-game/static/js/runtime~main.e6a6bc80.js"
+  }
+]);
